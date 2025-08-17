167ad9b9ed89fdec6f562b68b72e86382f559af7
lucasDeAraujoSilva <lucasaraujosilvamk@gmail.com>
2025-08-16 22:53:45 -0300

    adiciona string para feature

--- cypress/e2e/apagarDadosTabela.feature
+++ cypress/e2e/apagarDadosTabela.feature
@@ -5,5 +5,5 @@ Funcionalidade: DeletarDadoTabela-01 - Deletar dados na tabela
 
     Cenário:
         Dado que acesso o portal demoqa webtables
-        Quando clico no botão Delete de Action
-        Então os dados devem ser removidos corretamente da tabela
+        Quando clico no botão Delete de Action da linha "2"
+        Então os dados da linha dois devem ser removidos corretamente da tabela
--- cypress/support/step-definitions/apagarDadosTabela.cy.js
+++ cypress/support/step-definitions/apagarDadosTabela.cy.js
@@ -1,9 +1,10 @@
 import { When, Then } from "cypress-cucumber-preprocessor/steps"
 
-When("clico no botão Delete de Action", () => {
+When("clico no botão Delete de Action da linha {string}", (deletaLinha) => {
+   Cypress.env("deletaLinha", deletaLinha)
   cy.botaoDeletar()
 })
 
-Then("os dados devem ser removidos corretamente da tabela", () => {
+Then("os dados da linha dois devem ser removidos corretamente da tabela", () => {
   cy.botaoDeletar1()
 })
